734ed7859dd7df58c11acac31b8a1e81761c1576
Wojciech Urbański <woid@pm.me>
2019-03-07 11:36:07 +0100

    Add test messages and refactor fixtures

--- tests/fixtures/url_responses.py
+++ tests/fixtures/url_responses.py
@@ -1,4 +1,5 @@
 import re, json, os
+import responses
 from urllib.parse import urlparse
 from tests import FIXTURES_DIR
 
@@ -6,18 +7,32 @@ prefix = '/api/v1.1'
 
 # '/endpoint': (status, headers, response_body_filename)
 endpoints = {
-    '/me': (200, {}, 'me.json'),
-    '/projects': (200, {}, 'projects.json'),
-    '/project/((\w+)*)/((\w+)*)/((\w+)*)/((\w+)*)/((\w+)*)/((\w+)*)': (200, {}, 'log_example'),
-    '/project/((\w+)*)/((\w+)*)/((\w+)*)/((\w+)*)': (200, {}, 'build.json'),
+    '/me': 
+        { 'ret_code': 200, 'headers': {}, 'body': 'me.json'},
+    '/projects': 
+        { 'ret_code': 200, 'headers': {}, 'body': 'projects.json'},
+    # /project/:vcs_type/:username/:project/:build_num/output/:step/:index
+    '/project/((\w+)*)/((\w+)*)/((\w+)*)/((\w+)*)/((\w+)*)/((\w+)*)': 
+        { 'ret_code': 200, 'headers': {}, 'body': 'log_example'},
+    # /project/:vcs_type/:username/:project/:build_num
+    '/project/((\w+)*)/((\w+)*)/((\w+)*)/((\w+)*)': 
+        { 'ret_code': 200, 'headers': {}, 'body': 'build.json'},
 }
+
+def register_callback():
+    responses.add_callback(
+        responses.GET,
+        re.compile('https://circleci.com/api/v1.1/.*'),
+        callback=cached_response
+    )
+
 
 def cached_response(request):
     parsed_uri = urlparse(request.url)
     request_endpoint = parsed_uri.path[len(prefix):]
 
-    for endpoint, values in endpoints.items():
+    for endpoint, definition in endpoints.items():
         if re.match(endpoint, request_endpoint):
-            filename = os.path.join(FIXTURES_DIR, values[2])
+            filename = os.path.join(FIXTURES_DIR, definition['body'])
             with open(filename) as body:
-                return values[0], values[1], body.read()
+                return definition['ret_code'], definition['headers'], body.read()
